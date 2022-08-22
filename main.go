b9942f723063ca542a7cc80730efc4df2677c6bb
Mughees Ahmed <Mughees52@gmail.com>
2022-08-22 14:38:19 +0500

    Update main.go

--- main.go
+++ main.go
@@ -9,11 +9,12 @@ func main() {
 	var confenrenceNAme = "go confrence"
 	const tciekts = 50
 	var remaimaingTicket = 50
-
+        remaimaingTicket++
 	fmt.Println("this is the first program I wrote in the go")
 
 	fmt.Println(confenrenceNAme, "is live now ")
 	fmt.Println("we have totral of ", tciekts, ":")
 	fmt.Println("we have below ", remaimaingTicket, " of tickets ")
 
+	fmt.Println("we have below ", remaimaingTicket, " of tickets ")
 }
