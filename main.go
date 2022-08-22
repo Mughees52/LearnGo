c845fc5485d8718d64837ba6e82641b711b4f1f6
Mughees Ahmed <mughees52@gmail.com>
2022-08-22 14:10:13 +0500

    files added

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var confenrenceNAme = "go confrence"
+	const tciekts = 50
+	var remaimaingTicket = 50
+
+	fmt.Println("this is the first program I wrote in the go")
+
+	fmt.Println(confenrenceNAme, "is live now ")
+	fmt.Println("we have totral of ", tciekts, ":")
+	fmt.Println("we have below ", remaimaingTicket, " of tickets ")
+
+}
